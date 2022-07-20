7fffba59e50b32c3771001ae88caa221ebb1cd1d
Chynna <chynnaarceno@gmail.com>
2022-07-20 13:52:43 -0700

    Added view controllers and segues

---
+++ Prodactive/energyLevelsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  energyLevelsViewController.swift
+//  Prodactive
+//
+//  Created by Chynna Arceno on 7/20/22.
+//
+
+import UIKit
+
+class energyLevelsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
