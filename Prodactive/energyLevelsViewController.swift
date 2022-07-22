990798c9fb2bfe09c8d3fae60920ed42433f5307
Chynna <chynnaarceno@gmail.com>
2022-07-21 18:12:34 -0700

    Added date picker to energy level page

--- Prodactive/energyLevelsViewController.swift
+++ Prodactive/energyLevelsViewController.swift
@@ -9,13 +9,63 @@ import UIKit
 
 class energyLevelsViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    let datePicker = UIDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        createDatePicker()
     }
     
-
+    func createDatePicker() {
+        textField.textAlignment = .center
+        
+        //toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        //bar button
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([doneBtn], animated: true)
+        
+        //assign toolbar
+        textField.inputAccessoryView = toolbar
+        
+        //assign date picker to the text field
+        textField.inputView = datePicker
+        
+        //date picker mode
+        datePicker.datePickerMode = .date
+        
+        //adjust date picker frame
+        datePicker.frame.size = CGSize(width: 0, height: 200)
+    }
+    
+    @objc func donePressed() {
+        //formatter
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        textField.text = formatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.destination is productiveViewController {
+            let vc = segue.destination as? productiveViewController
+            vc?.date = textField.text ?? ""
+            
+        }
+    }
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
--- Prodactive/productiveViewController.swift
+++ Prodactive/productiveViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class productiveViewController: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var date: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        dateLabel.text = date
     }
     
 
